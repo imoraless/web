1c58eb8abc1c83eefcf5b54a8673f235338a1192
imoraless <72624897+imoraless@users.noreply.github.com>
2022-11-15 01:02:35 +0100

    Update BD_connect.php

--- model/BD_connect.php
+++ model/BD_connect.php
@@ -1,5 +1,5 @@
 <?php
-   // $conn = pg_connect("host=127.0.0.1 dbname=tdiw-a12 user=tdiw-a12 password=AoNFEosr");
+   // $conn = pg_connect("host=127.0.0.1 dbname=tdiwa12 user=tdiw-a12 password=AoNFEosr");
 
    $opciones[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
    $instancia = new PDO('mysql:host=localhost;dbname=bd_web', 'root' ,'', $opciones);
